3d1a3fd24fafc24f3d64776228c0db86ce4000c2
J1-ZONE <kingplays31@gmail.com>
2026-06-11 10:54:14 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "menda",
+        name: "Działa na Pierogach",
         // Activity type number (0 = Playing).
         type: 0, 
       },
@@ -88,7 +88,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
+      primary: "#FF0000", 
       secondary: "#2F3136", 
 
       // Standard status colors for success/error/warning/info messages.
